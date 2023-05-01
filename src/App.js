14978598d9561bd6d2e87b814ed8029ea1fc09df
333811 <mnawan.bese20seecs@seecs.edu.pk>
2023-05-01 04:40:03 -0700

    rename

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Forms from "./pages/Forms";
 import LandingPage from "./pages/LandingPage";
-import Calender from "./pages/Calender";
+import Calendar from "./pages/Calendar";
 import MyTasks from "./pages/MyTasks";
 import {Routes, Route} from "react-router-dom";
 
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/Login" element={<Forms />} />
-      <Route path="/Calender" element={<Calender />} />
+      <Route path="/Calender" element={<Calendar />} />
       <Route path="/MyTasks" element={<MyTasks />} />
     </Routes>
   );
