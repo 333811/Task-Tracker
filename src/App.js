9a01eadd9b47fd6e8daa185a92d3762b3e48b0b5
Khubaib Ahmad Qureshy <khubaib.qureshy@gmail.com>
2023-05-09 08:07:46 +0500

    nothig special

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Forms from "./pages/Forms";
 import LandingPage from "./pages/LandingPage";
 import Calendar from "./pages/Calendar";
 import MyTasks from "./pages/MyTasks";
+import TaskList from "./pages/TaskList";
 import {Routes, Route} from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/Login" element={<Forms />} />
       <Route path="/Calender" element={<Calendar />} />
       <Route path="/MyTasks" element={<MyTasks />} />
+      <Route path="/TaskList" element={<TaskList />} />
     </Routes>
   );
 }
